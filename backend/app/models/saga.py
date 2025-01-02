26bc5004fdbf79a6004380e06e48e056ac903e0c
Julio Varas Contreras <jvarascontreras@gmail.com>
2025-01-02 17:43:09 -0300

    - Se actualizó el modelo de saga y las rutas para usar ID de cadena en lugar de números enteros y se agregó validación y documentación adicionales.
    - Updated saga model and routes to use string IDs instead of integers and added additional validation and documentation

--- backend/app/models/saga.py
+++ backend/app/models/saga.py
@@ -1,8 +1,9 @@
-from pydantic import BaseModel, Field, ConfigDict
+from pydantic import BaseModel, Field, ConfigDict,field_validator
 from typing import Optional
+from bson import ObjectId
 
 class Saga(BaseModel):
-    id: Optional[int] = Field(alias="_id")
+    id: Optional[str] = Field(alias="_id")
     title: str
     saga_number: Optional[str] = None
     saga_chapitre: Optional[str] = None
@@ -12,3 +13,10 @@ class Saga(BaseModel):
     model_config: ConfigDict = {
         'populate_by_name': True  # Nueva forma de configurar
     }
+
+    # Validación adicional para convertir ObjectId a str
+    @field_validator("id", mode="before")
+    def convert_object_id(cls, value):
+        if isinstance(value, ObjectId):
+            return str(value)
+        return value
--- backend/app/routes/sagasRoutes.py
+++ backend/app/routes/sagasRoutes.py
@@ -1,28 +1,52 @@
 from fastapi import APIRouter, HTTPException, Depends
 from app.db.mongo import mongodb, object_id
 from app.models.saga import Saga
+from typing import List
+
 
 router = APIRouter()
 
-@router.post("/sagas", response_model=Saga, summary="Create a new saga")
+@router.post("/", response_model=Saga, summary="Create a new saga")
 async def create_saga(saga: Saga):
     result = await mongodb.db.sagas.insert_one(saga.model_dump(by_alias=True))
     saga.id = str(result.inserted_id)
     return saga
 
-@router.get("/sagas", summary="Get all sagas")
+@router.get("/", summary="Obtener todas las Sagas",
+        description="Lista todas las sagas que se encuentran en la BBDD.",
+        response_model=List[Saga],
+        responses={
+        200: {
+            "description": "Lista de sagas disponibles.",
+            "content": {
+                "application/json": {
+                    "example": [
+                        {
+                            "_id": "63f4e16e2b7c9d1b9df5b87f",
+                            "title": "East Blue",
+                            "saga_number": "1",
+                            "saga_chapitre": "1 à 100",
+                            "saga_volume": "1 à 12",
+                            "saga_episode": "1 à 61"
+                        }
+                    ]
+                }
+            }
+        },
+        404: {"description": "No existen sagas en la base datos."},
+    },)
 async def get_all_sagas():
     sagas = await mongodb.db.sagas.find().to_list(100)
-    return sagas
+    return [Saga(**saga) for saga in sagas]
 
-@router.get("/sagas/{id}", response_model=Saga, summary="Get a saga by ID")
+@router.get("/{id}", response_model=Saga, summary="Get a saga by ID")
 async def get_saga(id: str):
     saga = await mongodb.db.sagas.find_one({"_id": object_id(id)})
     if not saga:
         raise HTTPException(status_code=404, detail="Saga not found")
     return saga
 
-@router.put("/sagas/{id}", response_model=Saga, summary="Update a saga")
+@router.put("/{id}", response_model=Saga, summary="Update a saga")
 async def update_saga(id: str, saga: Saga):
     result = await mongodb.db.sagas.replace_one({"_id": object_id(id)}, saga.model_dump(by_alias=True))
     if result.matched_count == 0:
@@ -30,9 +54,46 @@ async def update_saga(id: str, saga: Saga):
     saga.id = id
     return saga
 
-@router.delete("/sagas/{id}", summary="Delete a saga")
+@router.delete("/{id}", summary="Delete a saga")
 async def delete_saga(id: str):
     result = await mongodb.db.sagas.delete_one({"_id": object_id(id)})
     if result.deleted_count == 0:
         raise HTTPException(status_code=404, detail="Saga not found")
     return {"message": "Saga deleted"}
+
+
+#region CODIGO COMENTADO PARA PROBAR TODOS LOS ENDPOINTS DE SAGAS
+# @router.post("/sagas", response_model=Saga, summary="Create a new saga")
+# async def create_saga(saga: Saga):
+#     result = await mongodb.db.sagas.insert_one(saga.model_dump(by_alias=True))
+#     saga.id = str(result.inserted_id)
+#     return saga
+
+# @router.get("/sagas", summary="Get all sagas")
+# async def get_all_sagas():
+#     sagas = await mongodb.db.sagas.find().to_list(100)
+#     return sagas
+
+# @router.get("/sagas/{id}", response_model=Saga, summary="Get a saga by ID")
+# async def get_saga(id: str):
+#     saga = await mongodb.db.sagas.find_one({"_id": object_id(id)})
+#     if not saga:
+#         raise HTTPException(status_code=404, detail="Saga not found")
+#     return saga
+
+# @router.put("/sagas/{id}", response_model=Saga, summary="Update a saga")
+# async def update_saga(id: str, saga: Saga):
+#     result = await mongodb.db.sagas.replace_one({"_id": object_id(id)}, saga.model_dump(by_alias=True))
+#     if result.matched_count == 0:
+#         raise HTTPException(status_code=404, detail="Saga not found")
+#     saga.id = id
+#     return saga
+
+# @router.delete("/sagas/{id}", summary="Delete a saga")
+# async def delete_saga(id: str):
+#     result = await mongodb.db.sagas.delete_one({"_id": object_id(id)})
+#     if result.deleted_count == 0:
+#         raise HTTPException(status_code=404, detail="Saga not found")
+#     return {"message": "Saga deleted"}
+
+#endregion
